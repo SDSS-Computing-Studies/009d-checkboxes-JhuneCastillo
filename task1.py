05b49bd8d9651f5278800d704142ff036b59214b
JhuneCastillo <71406970+JhuneCastillo@users.noreply.github.com>
2020-11-05 13:05:16 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -16,17 +16,18 @@ here
 Use assignment_test.py to test your functions
 """
 
-
-import tkinter as tk 
-
+import tkinter as tk
+from tkinter import *
 
 
 def binary_to_decimal(binary):
     # binary is a tuple of length 8
     # return value is an integer decimal
+    numbers = (binary[0] + binary[1] + binary[2] + binary[3] +
+               binary[4] + binary[5] + binary[6] + binary[7])
+    decimal = int(numbers, 2)
+    return decimal
 
-
-    return decimal 
 
 def decimal_to_binary(decimal):
     # decimal is an integer value
@@ -39,28 +40,81 @@ def get_binary():
     # function should read the entry widget and generate an integer
     # this integer will be used as an input parameter for decimal to binary and the result updated
     # in the 8 checkboxes
+    decimal = int(e1.get())
 
     binary = binary_to_decimal(decimal)
+    print(binary)
 
 
 def get_decimal():
     # function should read the checkboxes and generate a tuple called binary of length 8 that has 1's and 0's
     # this tuple will be used as an input parameter for binary_to_decimal and the result updated
     # in the entry box
-    binary = []
+
+    x1 = str(v1.get())
+    x2 = str(v2.get())
+    x3 = str(v3.get())
+    x4 = str(v4.get())
+    x5 = str(v5.get())
+    x6 = str(v6.get())
+    x7 = str(v7.get())
+    x8 = str(v8.get())
+
+    binary = [x1, x2, x3, x4, x5, x6, x7, x8]
     decimal = binary_to_decimal(binary)
 
+    oBox.set(decimal)
 
 
 win = tk.Tk()
 
+v1 = IntVar()
+v2 = IntVar()
+v3 = IntVar()
+v4 = IntVar()
+v5 = IntVar()
+v6 = IntVar()
+v7 = IntVar()
+v8 = IntVar()
 
+oBox = StringVar()
+oBox.set("")
 
-b1 = Button(win, text="Convert to Binary", command=get_binary)
-b2 = Button(win, text="Convert to Decimal", command=get_decimal)
+l1 = Label(win, text="Binary/Decimal Converter!")
 
+cF = Frame()
+c1 = Checkbutton(cF, onvalue=1, offvalue=0, variable=v1)
+c2 = Checkbutton(cF, onvalue=1, offvalue=0, variable=v2)
+c3 = Checkbutton(cF, onvalue=1, offvalue=0, variable=v3)
+c4 = Checkbutton(cF, onvalue=1, offvalue=0, variable=v4)
+c5 = Checkbutton(cF, onvalue=1, offvalue=0, variable=v5)
+c6 = Checkbutton(cF, onvalue=1, offvalue=0, variable=v6)
+c7 = Checkbutton(cF, onvalue=1, offvalue=0, variable=v7)
+c8 = Checkbutton(cF, onvalue=1, offvalue=0, variable=v8)
 
+e1 = Entry(win, textvariable=oBox)
 
+bF = Frame()
+b1 = Button(bF, text="Convert to Binary", command=get_binary)
+b2 = Button(bF, text="Convert to Decimal", command=get_decimal)
+
+l1.pack()
+
+cF.pack()
+c1.pack(side=LEFT)
+c2.pack(side=LEFT)
+c3.pack(side=LEFT)
+c4.pack(side=LEFT)
+c5.pack(side=LEFT)
+c6.pack(side=LEFT)
+c7.pack(side=LEFT)
+c8.pack(side=LEFT)
+
+bF.pack()
+b1.pack(side=LEFT)
+b2.pack(side=LEFT)
+
+e1.pack(side=LEFT)
 
 
 win.mainloop()
